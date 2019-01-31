df324119b6ea475e75b301f9af1f3718127aae51
zhush <andyjavadream@163.com>
2019-01-31 10:24:55 +0800

    blogger-jpa

---
+++ src/main/java/com/zhush/myblogger/entity/User.java
@@ -0,0 +1,14 @@
+package com.zhush.myblogger.entity;
+
+/**
+ * @author zhush
+ * @create 2019-01-31 10:13
+ */
+public class User {
+
+    public String username;
+
+    public String password;
+    
+
+}
